496b9eb3d4612482f74e3f8eae135f6f1682f832
Daniel Micay <danielmicay@gmail.com>
2019-01-13 10:18:24 +0100

    add toggle for disabling newly added USB devices
    Based on the public grsecurity patches.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -57,6 +57,8 @@
  */
 #define USB_SS_PORT_U0_WAKE_TIME	200  /* ms */
 
+extern int deny_new_usb;
+
 /* Protect struct usb_device->state and ->children members
  * Note: Both are also protected by ->dev.sem, except that ->state can
  * change to USB_STATE_NOTATTACHED even when the semaphore isn't held. */
@@ -5124,6 +5126,11 @@ static void hub_port_connect(struct usb_hub *hub, int port1, u16 portstatus,
 			goto done;
 		return;
 	}
+	if (deny_new_usb) {
+		dev_err(&port_dev->dev, "denied insert of USB device on port %d\n", port1);
+		goto done;
+	}
+
 	if (hub_is_superspeed(hub->hdev))
 		unit_load = 150;
 	else
