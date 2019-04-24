46237ef9f6ad4e1b9d48437a30404948519ad993
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2019-04-24 18:20:28 +0100

    usb: renesas_usbhs: Add reset_control
    commit f181dbb4824130e84f46e5be5b49cf6456f96683 upstream.
    
    R-Car Gen3 needs to deassert resets of both host and peripheral.
    Since [eo]hci-platform is possible to assert the reset(s) when
    the probing failed, renesas_usbhs driver doesn't work correctly
    regardless of finished probing. To fix this issue, this patch adds
    reset_control on this renesas_usbhs driver.
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Fabrizio Castro <fabrizio.castro@bp.renesas.com>
    Signed-off-by: Pavel Machek <pavel@denx.de>

--- drivers/usb/renesas_usbhs/common.h
+++ drivers/usb/renesas_usbhs/common.h
@@ -10,6 +10,7 @@
 
 #include <linux/extcon.h>
 #include <linux/platform_device.h>
+#include <linux/reset.h>
 #include <linux/usb/renesas_usbhs.h>
 
 struct usbhs_priv;
@@ -279,6 +280,7 @@ struct usbhs_priv {
 	struct usbhs_fifo_info fifo_info;
 
 	struct phy *phy;
+	struct reset_control *rsts;
 };
 
 /*
