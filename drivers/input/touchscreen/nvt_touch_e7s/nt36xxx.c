51c88a9a6a3877f1d8975155da93d3e883c63ff9
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-28 14:11:48 +0900

    touchscreen: nvt_touch_e7s: remove IRQ toggling under IRQ handler
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/input/touchscreen/nvt_touch_e7s/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_e7s/nt36xxx.c
@@ -1140,7 +1140,6 @@ static void nvt_ts_work_func(struct work_struct *work)
 	input_sync(ts->input_dev);
 
 out:
-	enable_irq(ts->client->irq);
 	mutex_unlock(&ts->lock);
 }
 
@@ -1153,7 +1152,6 @@ return:
 *******************************************************/
 static irqreturn_t nvt_ts_irq_handler(int32_t irq, void *dev_id)
 {
-	disable_irq_nosync(ts->client->irq);
 
 #if WAKEUP_GESTURE
 	if (unlikely(bTouchIsAwake == 0)) {
