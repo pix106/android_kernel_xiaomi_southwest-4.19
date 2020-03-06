bd719506abc0045af28b4f1934561bab3fb575bd
1582130940 <1582130940@qq.com>
2020-03-06 10:14:39 +0800

    mdss_dsi_panel: Drop hardware_info
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -25,10 +25,6 @@
 #include "mdss_dba_utils.h"
 #include "mdss_debug.h"
 
-#ifdef CONFIG_MACH_XIAOMI_CLOVER
-#include <linux/hardware_info.h>
-#endif
-
 #define DT_CMD_HDR 6
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 
@@ -3706,10 +3702,6 @@ int mdss_dsi_panel_init(struct device_node *node,
 	} else {
 		pr_info("%s: Panel Name = %s\n", __func__, panel_name);
 		strlcpy(&pinfo->panel_name[0], panel_name, MDSS_MAX_PANEL_LEN);
-
-#ifdef CONFIG_MACH_XIAOMI_CLOVER
-		get_hardware_info_data(HWID_LCM, &pinfo->panel_name[0]);
-#endif
 	}
 #ifdef CONFIG_MACH_LONGCHEER
 	if (strstr(panel_name,"tianma") == NULL)
