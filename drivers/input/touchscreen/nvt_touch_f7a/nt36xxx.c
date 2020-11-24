ea5913d14674785d8fabfa610840ac985fe0f673
subhajeetmuhuri <subhajeet.muhuri@gmail.com>
2020-11-24 17:06:32 +0000

    drivers: touchscreen: nvt_touch_f7a: Update wakeup_source APIs for 4.19
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
@@ -714,7 +714,7 @@ static int32_t nvt_flash_proc_init(void)
 /* function page definition */
 #define FUNCPAGE_GESTURE         1
 
-static struct wakeup_source gesture_wakeup;
+static struct wakeup_source *gesture_wakeup;
 
 /*******************************************************
 Description:
@@ -1166,7 +1166,7 @@ static irqreturn_t nvt_ts_irq_handler(int32_t irq, void *dev_id)
 
 #if WAKEUP_GESTURE
 	if (bTouchIsAwake == 0) {
-		__pm_wakeup_event(&gesture_wakeup, msecs_to_jiffies(5000));
+		__pm_wakeup_event(gesture_wakeup, msecs_to_jiffies(5000));
 	}
 #endif
 
@@ -1461,7 +1461,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++) {
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
 	}
-	wakeup_source_init(&gesture_wakeup, "poll-wake-lock");
+	gesture_wakeup = wakeup_source_register(NULL, "poll-wake-lock");
 #ifdef CONFIG_TOUCHSCREEN_COMMON
     ret = tp_common_set_double_tap_ops(&double_tap_ops);
     if (ret < 0) {
