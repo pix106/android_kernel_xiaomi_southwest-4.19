18b5801423cc0f637d4d6d7842d2b2f9a2c4e5be
Yaroslav Furman <yaro330@gmail.com>
2020-12-11 19:22:31 +0300

    printk: add userspace messages filter
    Some messages get too annoying.
    
    * only add cacert case + free reference
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -826,12 +826,14 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			len -= endp - line;
 			line = endp;
 			if (strstr(line, "healthd") ||
+				strstr(line, "cacert") ||
 				strncmp(line, "logd: Skipping", sizeof("logd: Skipping")))
-				return ret;
+				goto free;
 		}
 	}
 
 	printk_emit(facility, level, NULL, 0, "%s", line);
+free:
 	return ret;
 }
 
