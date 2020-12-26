63801b8716fcd2d4ad9a634ec7674c4889266ece
Manish4586 <manish.n.manish45@gmail.com>
2020-12-26 11:34:43 +0530

    drivers: power: properly import missing xiaomi changes
    Bug:* in some cases after a kernel panic battery temperature gets stucked at 40`c or more
    to fix this issue we need to reboot the device from fastboot or a warm start after full poweroff/ shutdown
    
    Noticed On: Mi A2/6X, Mi Note 3, Redmi Note 6 Pro
    
    Change-Id: Idccbbe5b7cbb0d848f75220a06f160b3126a915b
    (cherry picked from commit 3a6ba8d2832b84227844d7437ae414a9b9a0317d)
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/power/supply/qcom/fg-reg.h
+++ drivers/power/supply/qcom/fg-reg.h
@@ -293,7 +293,7 @@
 /* BATT_INFO_PEEK_MUX1 */
 #define PEEK_MUX1_BIT				BIT(0)
 
-#ifdef CONFIG_MACH_LONGCHEER
+#if defined(CONFIG_MACH_LONGCHEER) || defined(CONFIG_MACH_XIAOMI_CLOVER)
 #define MEM_IF_MEM_ARB_CFG(chip)		((chip->mem_if_base) + 0x40)
 #define MEM_GNT_BIT				BIT(2)
 #endif
@@ -374,7 +374,7 @@
 #define ADDR_KIND_BIT				BIT(1)
 #define DMA_CLEAR_LOG_BIT			BIT(0)
 
-#ifdef CONFIG_MACH_LONGCHEER
+#if defined(CONFIG_MACH_LONGCHEER) || defined(CONFIG_MACH_XIAOMI_CLOVER)
 /* MEM_IF_REQ */
 #define MEM_IF_ARB_REQ_BIT			BIT(0)
 #endif
