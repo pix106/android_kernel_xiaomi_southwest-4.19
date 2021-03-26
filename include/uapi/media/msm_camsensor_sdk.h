cbb7424b20a501a8d7fa3030addabfa95a46440f
OdSazib <odsazib@gmail.com>
2021-03-26 10:55:59 +0600

    msm: camera_v2: Fix newCam structs for wayne blobs
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -334,9 +334,8 @@ struct msm_vcm_id_info_t {
 	enum msm_camera_i2c_data_type data_type;
 	enum msm_cci_i2c_master_t cci_i2c_master;
 };
-#endif
 
-#if defined(CONFIG_MACH_XIAOMI_NEW_CAMERA) || defined(CONFIG_MACH_XIAOMI_WAYNE)
+#ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
 struct msm_lens_id_info_t {
 	unsigned short eeprom_slave_addr;
 	unsigned short lens_id_addr;
@@ -345,6 +344,7 @@ struct msm_lens_id_info_t {
 	enum msm_camera_i2c_data_type data_type;
 	enum msm_cci_i2c_master_t cci_i2c_master;
 };
+#endif
 #endif
 
 struct msm_camera_sensor_slave_info {
@@ -358,23 +358,18 @@ struct msm_camera_sensor_slave_info {
 	enum i2c_freq_mode_t i2c_freq_mode;
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	struct msm_sensor_id_info_t sensor_id_info;
-#if defined(CONFIG_MACH_XIAOMI_SDM660) && !defined(CONFIG_MACH_XIAOMI_WAYNE)
+#ifdef CONFIG_MACH_XIAOMI_SDM660
 	struct msm_vendor_id_info_t vendor_id_info;
 	struct msm_vcm_id_info_t vcm_id_info;
-#endif
 #ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
 	struct msm_lens_id_info_t lens_id_info;
+#endif
 #endif
 	struct msm_sensor_power_setting_array power_setting_array;
 	unsigned char  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
 	enum msm_sensor_output_format_t output_format;
 	uint8_t bypass_video_node_creation;
-#ifdef CONFIG_MACH_XIAOMI_WAYNE
-	struct msm_vendor_id_info_t vendor_id_info;
-	struct msm_vcm_id_info_t vcm_id_info;
-	struct msm_lens_id_info_t lens_id_info;
-#endif
 };
 
 struct msm_camera_i2c_reg_array {
