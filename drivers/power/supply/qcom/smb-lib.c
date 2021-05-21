8dc50557d43e77eabad780b8b6ddb7aed7627129
Nick C <ctwoon@suicide.today>
2021-05-22 01:34:40 +0800

    dts: drivers: increase charging speed
    * Add missing stuffs
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -3037,23 +3037,36 @@ int smblib_get_prop_die_health(struct smb_charger *chg,
 	return 0;
 }
 
-#define SDP_CURRENT_UA			500000
-#define CDP_CURRENT_UA			1500000
-#ifdef CONFIG_MACH_LONGCHEER
-#define DCP_CURRENT_UA			2000000
-#define HVDCP2_CURRENT_UA		1500000
-#if defined(CONFIG_MACH_XIAOMI_WHYRED) || defined(CONFIG_MACH_XIAOMI_TULIP)
-#define HVDCP_CURRENT_UA		2000000
+#define SDP_CURRENT_UA			3300000
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+#define CDP_CURRENT_UA			3300000
 #else
-#define HVDCP_CURRENT_UA		2900000
+#define CDP_CURRENT_UA			3300000
 #endif
+#ifdef CONFIG_MACH_LONGCHEER
+#define DCP_CURRENT_UA			3300000
+#define HVDCP2_CURRENT_UA		3300000
+#if defined(CONFIG_MACH_XIAOMI_WHYRED) || defined(CONFIG_MACH_XIAOMI_TULIP)
+#define HVDCP_CURRENT_UA		3300000
 #else
-#define DCP_CURRENT_UA			1500000
-#define HVDCP_CURRENT_UA		3000000
+#define HVDCP_CURRENT_UA		3300000
+#endif
+#elif defined (CONFIG_MACH_MI)
+#define DCP_CURRENT_UA			3300000
+#define DCP_CURRENT_UA_2A		3300000
+#define HVDCP_CURRENT_UA		3350000
+#define HVDCP2_CURRENT_UA		3300000
+#else
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+#define DCP_CURRENT_UA			3300000
+#else
+#define DCP_CURRENT_UA			3300000
+#endif
+#define HVDCP_CURRENT_UA	3300000
 #endif
 #define TYPEC_DEFAULT_CURRENT_UA	900000
 #define TYPEC_MEDIUM_CURRENT_UA		1500000
-#define TYPEC_HIGH_CURRENT_UA		3000000
+#define TYPEC_HIGH_CURRENT_UA		3300000
 static int get_rp_based_dcp_current(struct smb_charger *chg, int typec_mode)
 {
 	int rp_ua;
@@ -4338,7 +4351,7 @@ static void smblib_force_legacy_icl(struct smb_charger *chg, int pst)
 #if defined(CONFIG_MACH_XIAOMI_WAYNE) || defined(CONFIG_MACH_XIAOMI_LAVENDER)
 		vote(chg->usb_icl_votable, USER_VOTER, false, 0);
 #endif
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 1500000);
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 		break;
 	case POWER_SUPPLY_TYPE_USB_DCP:
 #if defined(CONFIG_MACH_XIAOMI_WAYNE) || defined(CONFIG_MACH_XIAOMI_LAVENDER)
@@ -4358,22 +4371,22 @@ static void smblib_force_legacy_icl(struct smb_charger *chg, int pst)
 		vote(chg->usb_icl_votable, USER_VOTER, false, 0);
 #endif
 #ifdef CONFIG_MACH_XIAOMI_WHYRED
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 500000);
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 #else
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 1000000);
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 #endif
 #else
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 100000);
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 #endif
 		break;
 	case POWER_SUPPLY_TYPE_USB_HVDCP:
 #ifdef CONFIG_MACH_LONGCHEER
 #if defined(CONFIG_MACH_XIAOMI_TULIP)
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 2000000);
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 #elif defined(CONFIG_MACH_XIAOMI_WAYNE) || defined(CONFIG_MACH_XIAOMI_LAVENDER)
-		vote(chg->usb_icl_votable, USER_VOTER, true, 1500000);
+		vote(chg->usb_icl_votable, USER_VOTER, true, 3300000);
 #else
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 1500000);
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 #endif
 		break;
 #endif
@@ -4385,14 +4398,14 @@ static void smblib_force_legacy_icl(struct smb_charger *chg, int pst)
 		if (hwc_check_global)
 			vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 		else
-			vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 2900000);
+			vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 #else
 		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3000000);
 #endif
 		break;
 	default:
 		smblib_err(chg, "Unknown APSD %d; forcing 500mA\n", pst);
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 500000);
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
 		break;
 	}
 }
