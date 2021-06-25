1d2c5e985caa495252b4be07545e53e42393b2e5
Panchajanya1999 <panchajanya@azure-dev.live>
2021-06-25 15:20:08 +0530

    msm: kgsl: Place worker thread on SCHED_RR & WQ_HIGHPRI
    On commit 254852b
    (msm: kgsl: Increase worker thread priority), we had increased
    the priority for better rendering frames.
    
    Since a lot of display related tasks are multiplexed on KGSL on a
    dedicated thread, they are placed in a FIFO manner, which is a bit
    of time consuming since not all those queued tasks have a known or
    small execution period.
    
    switching to Round-Robin fashion eliminates this complexity and bounds
    these tasks on a time-slice and thus every tasks gets executed after
    a certain time, as fast as possible.
    
    Change-Id: I2b5137a5c6fdcc7fef7cafcc3e5728eab0034045
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5558,7 +5558,7 @@ static int __init kgsl_core_init(void)
 		WQ_HIGHPRI | WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_SYSFS, 0);
 
 	kgsl_driver.mem_workqueue = alloc_workqueue("kgsl-mementry",
-		WQ_UNBOUND | WQ_MEM_RECLAIM, 0);
+		WQ_HIGHPRI | WQ_UNBOUND | WQ_MEM_RECLAIM, 0);
 
 	INIT_WORK(&kgsl_driver.mem_work, _flush_mem_workqueue);
 
@@ -5572,7 +5572,7 @@ static int __init kgsl_core_init(void)
 		goto err;
 	}
 
-	sched_setscheduler(kgsl_driver.worker_thread, SCHED_FIFO, &param);
+	sched_setscheduler(kgsl_driver.worker_thread, SCHED_RR, &param);
 
 	kgsl_events_init();
 
