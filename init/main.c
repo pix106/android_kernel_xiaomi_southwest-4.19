a4fe7b918fd57e5ab6f9b342295c6a1d9c15143a
pix106 <sbordenave@gmail.com>
2021-07-21 08:15:40 +0200

    init: changes for clover
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- init/main.c
+++ init/main.c
@@ -552,7 +552,7 @@ static void __init mm_init(void)
 	pti_init();
 }
 
-#ifdef CONFIG_MACH_LONGCHEER
+#if defined(CONFIG_MACH_LONGCHEER) || defined(CONFIG_MACH_XIAOMI_CLOVER)
 int fpsensor = 1;
 bool is_poweroff_charge = false;
 #endif
