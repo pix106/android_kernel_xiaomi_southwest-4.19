df589ca5b7b51b64e092c65ad55420887a54a94c
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-07-05 14:05:53 +0600

    power: supply: Conditionally compile logging
    These spew quite the useless logs which we won't ever
    read and we don't them to be available on production builds.
    Disable them with an empty do-while loop if !DEBUG.
    
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -29,12 +29,18 @@ extern int hwc_check_global;
 #include <linux/fastchg.h>
 #endif
 
+#ifdef DEBUG
 #define smblib_err(chg, fmt, ...)		\
 	pr_err("%s: %s: " fmt, chg->name,	\
 		__func__, ##__VA_ARGS__)	\
 
 #define smblib_dbg(chg, reason, fmt, ...)			\
 	do { } while (0)
+
+#else
+#define smblib_err(chg, fmt, ...) do {} while (0)
+#define smblib_dbg(chg, reason, fmt, ...) do {} while (0)
+#endif
 
 #ifdef CONFIG_MACH_XIAOMI_LAVENDER
 struct g_nvt_data {
@@ -3977,6 +3983,7 @@ int smblib_get_prop_slave_current_now(struct smb_charger *chg,
  * INTERRUPT HANDLERS *
  **********************/
 
+#ifdef DEBUG
 irqreturn_t smblib_handle_debug(int irq, void *data)
 {
 	struct smb_irq_data *irq_data = data;
@@ -3985,6 +3992,12 @@ irqreturn_t smblib_handle_debug(int irq, void *data)
 	smblib_dbg(chg, PR_INTERRUPT, "IRQ: %s\n", irq_data->name);
 	return IRQ_HANDLED;
 }
+#else
+inline irqreturn_t smblib_handle_debug(int irq, void *data)
+{
+	return IRQ_HANDLED;
+}
+#endif
 
 irqreturn_t smblib_handle_otg_overcurrent(int irq, void *data)
 {
