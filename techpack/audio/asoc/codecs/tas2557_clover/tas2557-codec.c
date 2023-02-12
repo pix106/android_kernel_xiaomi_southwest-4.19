b14b217ead0210b17780bdbc02bb1282d6012790
Mohan Kumar <mkumard@nvidia.com>
2023-02-12 20:14:17 +0800

    tas2557: Support capture for tas2557 amp
    Add support for capture path in tas2557 driver.
    
    Bug 200395627
    
    Change-Id: I07bf1f74ffbd8f082025a3a642c542b93994a018
    Signed-off-by: Mohan Kumar <mkumard@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1668392
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Ravindra Lokhande <rlokhande@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Signed-off-by: ktommy91 <kemenesi.tamas@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- techpack/audio/asoc/codecs/tas2557_clover/tas2557-codec.c
+++ techpack/audio/asoc/codecs/tas2557_clover/tas2557-codec.c
@@ -119,7 +119,8 @@ static const struct snd_soc_dapm_widget tas2557_dapm_widgets[] = {
 	SND_SOC_DAPM_SUPPLY("Stereo PLL", SND_SOC_NOPM, 0, 0, NULL, 0),
 	SND_SOC_DAPM_SUPPLY("Stereo NDivider", SND_SOC_NOPM, 0, 0, NULL, 0),
 
-	SND_SOC_DAPM_OUTPUT("Stereo OUT")
+	SND_SOC_DAPM_OUTPUT("Stereo OUT"),
+	SND_SOC_DAPM_INPUT("Stereo IN")
 };
 
 static const struct snd_soc_dapm_route tas2557_audio_map[] = {
@@ -130,6 +131,7 @@ static const struct snd_soc_dapm_route tas2557_audio_map[] = {
 	{"Stereo OUT", NULL, "Stereo ClassD"},
 	{"Stereo DAC", NULL, "Stereo PLL"},
 	{"Stereo DAC", NULL, "Stereo NDivider"},
+	{"Stereo ASI1 Capture", NULL, "IN"},
 };
 
 static int tas2557_startup(struct snd_pcm_substream *substream,
@@ -891,6 +893,13 @@ static struct snd_soc_dai_driver tas2557_dai_driver[] = {
 				.rates = SNDRV_PCM_RATE_8000_192000,
 				.formats = TAS2557_FORMATS,
 			},
+		.capture = {
+			.stream_name = "Stereo ASI1 Capture",
+			.channels_min = 2,
+			.channels_max = 2,
+			.rates = SNDRV_PCM_RATE_8000_192000,
+			.formats = TAS2557_FORMATS,
+		},
 		.ops = &tas2557_dai_ops,
 		.symmetric_rates = 1,
 	},
