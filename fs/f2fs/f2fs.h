a95a9ea4afe6b210cb73ddc544275acd953063c0
pix106 <sbordenave@gmail.com>
2023-03-01 18:03:43 +0100

    Revert "f2fs: clean up /sys/fs/f2fs/<disk>/features"
    This reverts commit d5035ead19d22691e8f933fd0415e626dcb73c1f.

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1798,9 +1798,6 @@ struct f2fs_sb_info {
 	struct kobject s_stat_kobj;		/* /sys/fs/f2fs/<devname>/stat */
 	struct completion s_stat_kobj_unregister;
 
-	struct kobject s_feature_list_kobj;		/* /sys/fs/f2fs/<devname>/feature_list */
-	struct completion s_feature_list_kobj_unregister;
-
 	/* For shrinker support */
 	struct list_head s_list;
 	struct mutex umount_mutex;
