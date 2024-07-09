a36565f5280276e61952ea9e562a862d80dea27e
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-07-09 10:50:24 +0800

    treewide: Fix logspam with !IPC_LOGGING and !DEBUG_FS
    Co-Authored-By: Divyanshu-Modi <divyan.m05@gmail.com>
    Co-Authored-By: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Co-Authored-By: Cyber Knight <cyberknight755@gmail.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1467,9 +1467,6 @@ static int dwc3_probe(struct platform_device *pdev)
 	int					ret;
 	void __iomem		*regs;
 	int					irq;
-#ifdef CONFIG_IPC_LOGGING
-	char				dma_ipc_log_ctx_name[40];
-#endif
 
 	if (count >= DWC_CTRL_COUNT) {
 		dev_err(dev, "Err dwc instance %d >= %d available\n",
@@ -1652,7 +1649,9 @@ static int dwc3_remove(struct platform_device *pdev)
 {
 	struct dwc3	*dwc = platform_get_drvdata(pdev);
 
+#ifdef CONFIG_DEBUG_FS
 	dwc3_debugfs_exit(dwc);
+#endif
 	dwc3_gadget_exit(dwc);
 
 	pm_runtime_allow(&pdev->dev);
@@ -1662,10 +1661,12 @@ static int dwc3_remove(struct platform_device *pdev)
 	dwc3_free_scratch_buffers(dwc);
 	clk_bulk_put(dwc->num_clks, dwc->clks);
 
+#ifdef CONFIG_IPC_LOGGING
 	ipc_log_context_destroy(dwc->dwc_ipc_log_ctxt);
 	dwc->dwc_ipc_log_ctxt = NULL;
 	ipc_log_context_destroy(dwc->dwc_dma_ipc_log_ctxt);
 	dwc->dwc_dma_ipc_log_ctxt = NULL;
+#endif
 	count--;
 	dwc3_instance[dwc->index] = NULL;
 
