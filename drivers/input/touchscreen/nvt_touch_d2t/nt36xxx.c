4ebb0fbc8b31b836cd6ea9ab0aacb7e567a89fd9
Sabar <31942277+sabarop@users.noreply.github.com>
2024-07-18 16:49:43 +0700

    touchscreen: nvt_touch_d2t: simplify i2c functions
    And better locate bp hints
    
    Co-Authored-By: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/input/touchscreen/nvt_touch_d2t/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_d2t/nt36xxx.c
@@ -170,8 +170,8 @@ return:
 int32_t CTP_I2C_READ(struct i2c_client *client, uint16_t address, uint8_t *buf, uint16_t len)
 {
 	struct i2c_msg msgs[2];
-	int32_t ret = -1;
-	int32_t retries = 0;
+	int32_t ret;
+	int32_t retries;
 	msgs[0].flags = !I2C_M_RD;
 	msgs[0].addr  = address;
 	msgs[0].len   = 1;
@@ -181,23 +181,14 @@ int32_t CTP_I2C_READ(struct i2c_client *client, uint16_t address, uint8_t *buf, 
 	msgs[1].len   = len - 1;
 	msgs[1].buf   = &buf[1];
 
-	while (retries < 5) {
+	for (retries = 0; retries < 5; retries++) {
 		ret = i2c_transfer(client->adapter, msgs, 2);
-
-		if (ret == 2)
-			break;
-
-		retries++;
-		msleep(20);
-		NVT_ERR("error, retry=%d\n", retries);
+		if (likely(ret == 2))
+			return ret;
 	}
 
-	if (unlikely(retries == 5)) {
-		NVT_ERR("error, ret=%d\n", ret);
-		ret = -EIO;
-	}
-
-	return ret;
+	NVT_ERR("i2c read error\n");
+	return -EIO;
 }
 
 /*******************************************************
@@ -210,30 +201,21 @@ return:
 int32_t CTP_I2C_WRITE(struct i2c_client *client, uint16_t address, uint8_t *buf, uint16_t len)
 {
 	struct i2c_msg msg;
-	int32_t ret = -1;
-	int32_t retries = 0;
+	int32_t ret;
+	int32_t retries;
 	msg.flags = !I2C_M_RD;
 	msg.addr  = address;
 	msg.len   = len;
 	msg.buf   = buf;
 
-	while (retries < 5) {
+	for (retries = 0; retries < 5; retries++) {
 		ret = i2c_transfer(client->adapter, &msg, 1);
-
-		if (ret == 1)
-			break;
-
-		retries++;
-		msleep(20);
-		NVT_ERR("error, retry=%d\n", retries);
+		if (likely(ret == 1))
+			return ret;
 	}
 
-	if (unlikely(retries == 5)) {
-		NVT_ERR("error, ret=%d\n", ret);
-		ret = -EIO;
-	}
-
-	return ret;
+	NVT_ERR("i2c write error\n");
+	return -EIO;
 }
 
 
