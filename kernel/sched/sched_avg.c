867d3e5f099f4b93e8b443cf462b7bebe4a7efae
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2024-07-23 01:23:53 +0800

    kernel/sched/sched_avg: Increase nr_threshold to 40 percent
    Increase the nr_threshold percentage to 40 from 15.
    
    Change-Id: I32ce7246fc4cd32d4c8110bef63971c9a2dceb55
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

--- kernel/sched/sched_avg.c
+++ kernel/sched/sched_avg.c
@@ -33,7 +33,7 @@ unsigned int sysctl_sched_coloc_busy_hyst_max_ms = 5000;
 static DEFINE_PER_CPU(atomic64_t, busy_hyst_end_time) = ATOMIC64_INIT(0);
 static DEFINE_PER_CPU(u64, hyst_time);
 
-#define NR_THRESHOLD_PCT		15
+#define NR_THRESHOLD_PCT		40
 #define MAX_RTGB_TIME (sysctl_sched_coloc_busy_hyst_max_ms * NSEC_PER_MSEC)
 
 /**
