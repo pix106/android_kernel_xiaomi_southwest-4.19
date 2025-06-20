ddc52fb6e42ad5536a34269d0103ac60817ea073
Germanium Sculk <germaniumsculk@gmail.com>
2025-06-20 16:17:09 +0600

    Backport : fs/namespace.c path_umount (correction)

--- fs/namespace.c
+++ fs/namespace.c
@@ -1675,7 +1675,7 @@ int path_umount(struct path *path, int flags)
 	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
 	dput(path->dentry);
 	mntput_no_expire(mnt);
-	return ret;Add commentMore actions
+	return ret;
 }
 	
 /*
