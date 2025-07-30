8c67edbff54af5f647a8effb19f642c5d9a87b75
Nobuhiro Iwamatsu <nobuhiro1.iwamatsu@toshiba.co.jp>
2025-07-30 12:09:10 +0900

    emulex/benet: Fix build by return mismatch in be_cmd_unlock()
    be_cmd_unlock() has no return value. However, spin_unlock_bh is called
    with return, causing a build error. This removes return and fixes the error.
    
    ```
    $ make drivers/net/ethernet/emulex/benet/be_cmds.o
      CALL    scripts/checksyscalls.sh
      DESCEND  objtool
      CC      drivers/net/ethernet/emulex/benet/be_cmds.o
    In file included from ./include/linux/spinlock.h:317,
                     from ./include/linux/seqlock.h:36,
                     from ./include/linux/time.h:6,
                     from ./include/linux/stat.h:19,
                     from ./include/linux/module.h:10,
                     from drivers/net/ethernet/emulex/benet/be_cmds.c:18:
    drivers/net/ethernet/emulex/benet/be_cmds.c: In function ‘be_cmd_unlock’:
    ./include/linux/spinlock_rt.h:113:9: error: expected expression before ‘do’
      113 |         do {                                            \
          |         ^~
    drivers/net/ethernet/emulex/benet/be_cmds.c:883:24: note: in expansion of macro ‘spin_unlock_bh’
      883 |                 return spin_unlock_bh(&adapter->mcc_lock);
          |                        ^~~~~~~~~~~~~~
    ./include/linux/spinlock_rt.h:113:9: error: ‘return’ with a value, in function returning void [-Werror=return-type]
      113 |         do {                                            \
          |         ^~
    drivers/net/ethernet/emulex/benet/be_cmds.c:883:24: note: in expansion of macro ‘spin_unlock_bh’
      883 |                 return spin_unlock_bh(&adapter->mcc_lock);
          |                        ^~~~~~~~~~~~~~
    drivers/net/ethernet/emulex/benet/be_cmds.c:880:13: note: declared here
      880 | static void be_cmd_unlock(struct be_adapter *adapter)
          |             ^~~~~~~~~~~~~
    cc1: some warnings being treated as errors
    make[1]: *** [scripts/Makefile.build:304: drivers/net/ethernet/emulex/benet/be_cmds.o] Error 1
    make: *** [Makefile:1719: drivers/net/ethernet/emulex/benet/be_cmds.o] Error 2
    ```
    
    Fixes: 3ec3ccba308945d3f65a1adb0889aab016f1f12b ("be2net: fix sleeping while atomic bugs in be_ndo_bridge_getlink")
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro1.iwamatsu@toshiba.co.jp>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- drivers/net/ethernet/emulex/benet/be_cmds.c
+++ drivers/net/ethernet/emulex/benet/be_cmds.c
@@ -880,7 +880,7 @@ static int be_cmd_lock(struct be_adapter *adapter)
 static void be_cmd_unlock(struct be_adapter *adapter)
 {
 	if (use_mcc(adapter))
-		return spin_unlock_bh(&adapter->mcc_lock);
+		spin_unlock_bh(&adapter->mcc_lock);
 	else
 		return mutex_unlock(&adapter->mbox_lock);
 }
